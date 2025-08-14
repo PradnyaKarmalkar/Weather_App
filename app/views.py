37bb15a865581719f341da2ed29db6478057d510
PradnyaKarmalkar <pradnyakarmalkar081@gmail.com>
2025-08-14 20:09:30 +0530

    Working project

--- app/views.py
+++ app/views.py
@@ -6,6 +6,9 @@ def index(request):
     api_key = '70da8fd98121c1a65a9159c7bfb51568'
     
     api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    
+    print(api_url)  # Debugging line to check the API URL
+    
     api = requests.get(api_url).json()
     
     if api.get("cod") != 200:  # API returned an error
@@ -15,9 +18,25 @@ def index(request):
     temperature = api['main']['temp']
     city = api['name']
     country = api['sys']['country']
+    wind_speed = api['wind']['speed']
+    humidity = api['main']['humidity']
+    
+    icon_url =' https://openweathermap.org/img/wn/10d@2x.png'
+    print(icon_url)
+    icon_id=api['weather'][0]['icon']
+    weather=api['weather'][0]['main']
+    description=api['weather'][0]['description']
+    
+    
     
     return render(request, 'index.html', {
         'temperature': temperature,
         'city': city,
-        'country': country
+        'country': country,
+        'wind_speed': wind_speed,
+        'humidity': humidity,
+        'weather': weather,
+        'description': description,
+        'icon_url': icon_url,
+        
     })
